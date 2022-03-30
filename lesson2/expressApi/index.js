019b59c7dc53f3fff1e94b1e279cc616f8582c17
galamouyal <galamouyal88@gmail.com>
2022-03-30 18:42:40 +0300

    middleware

--- lesson2/expressApi/index.js
+++ lesson2/expressApi/index.js
@@ -6,30 +6,48 @@ const app = express()
 // app.put
 // app.post
 
+function validateUserExist(user) {
+    return user && typeof user === 'string'
+}
+
+function validateUserLength(user) {
+    return user.length > 5 && user.length < 100
+}
+
+app.use(requestStartedMiddleware)
+app.use((req, res, next) => {
+    console.log(req.url, "second middleware")
+    next()
+})
 
 app.get("/login", (req, res) => {
     const { userName } = req.query
-    if (typeof userName !== 'string') {
-        return res.status(400).json({ message: "invalid userName" })
-    } else {
-        console.log(`Request Started ${req.ip} - ${userName}`)
-        res.json({ message: `Login succeeded! for ${userName}` })
+    if (!validateUserExist(userName)) {
+        return res.status(400).json({ message: "userName not exist", date: new Date().toUTCString() })
     }
+    if (!validateUserLength(userName)) {
+        return res.status(400).json({ message: "userName is too short/long", date: new Date().toUTCString() })
+    }
+    console.log(`Request Started ${req.ip} - ${userName}`)
+    res.json({ message: `Login succeeded! for ${userName}` })
 })
 
 app.get("/register", (req, res) => {
     const { userName } = req.query
+    if (!validateUserExist(userName)) {
+        return res.status(400).json({ message: "userName not exist" })
+    }
     console.log(`Request Started ${req.ip} - ${userName}`)
     res.send("Rgister complete")
 })
 
-app.get("/login2", (req, res) => {
-    const { userName } = req.query
-    console.log(`Request Started ${req.ip} - ${userName}`)
-    res.json({ message: `Login2 succeeded! for ${userName}` })
-})
 
 app.listen(port).on("listening", function () {
     console.log(`Server is listen to Port ${port}`)
 })
 
+
+function requestStartedMiddleware(req, res, next) {
+    console.log(`Request Started ${req.url}`)
+    next()
+}
